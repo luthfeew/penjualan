7ec403502274382eef00f563052e0a6b5e9e11dc
luthfeew <luthfeew@gmail.com>
2023-01-13 02:16:09 +0700

    CONTROLLER (MODUL V)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\produkController;
+use App\Http\Controllers\pelangganController;
 
 /*
 |--------------------------------------------------------------------------
@@ -50,3 +51,5 @@ Route::get('/halaman', function () {
     $konten = 'harry potter and the deathly hallows: part 2';
     return view('halaman', compact('title', 'konten'));
 });
+
+Route::get('/pelanggan', [pelangganController::class, 'index']);
